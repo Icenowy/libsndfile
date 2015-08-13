cbd21e6795809e73334bb4b6e89f9729f7d2cc84
Icenowy Zheng <icenowy@outlook.com>
2015-08-13 10:54:00 +0800

    build: made some fix to build out-of-tree

--- CMake/build.cmake
+++ CMake/build.cmake
@@ -1,8 +1,8 @@
 # Build recipe for building programs in the programs/ directory.
 function (lsf_build_program prog_name extra_libs)
 	add_executable (${prog_name}
-		programs/common.c
-		programs/${prog_name}.c
+		${CMAKE_CURRENT_SOURCE_DIR}/programs/common.c
+		${CMAKE_CURRENT_SOURCE_DIR}/programs/${prog_name}.c
 		)
 	target_link_libraries (${prog_name} sndfile ${extra_libs})
 	set_target_properties (${prog_name}
@@ -15,7 +15,7 @@ function (lsf_build_program prog_name extra_libs)
 # Build recipe for building C tests in the src/ directory.
 function (lsf_build_src_test_c test_name extra_files)
 	add_executable (${test_name}
-		src/${test_name}.c
+		${CMAKE_CURRENT_SOURCE_DIR}/src/${test_name}.c
 		${extra_files}
 		)
 	target_link_libraries (${test_name} m)
@@ -31,8 +31,8 @@ function (lsf_build_src_test_c test_name extra_files)
 # Build recipe for building C tests in the tests/ directory.
 function (lsf_build_test_c test_name extra_files)
 	add_executable (${test_name}
-		tests/${test_name}.c
-		tests/utils.c
+		${CMAKE_CURRENT_SOURCE_DIR}/tests/${test_name}.c
+		${CMAKE_CURRENT_SOURCE_DIR}/tests/utils.c
 		${extra_files}
 		)
 	target_link_libraries (${test_name} sndfile)
@@ -48,8 +48,8 @@ function (lsf_build_test_c test_name extra_files)
 # Build recipe for building C++ tests in the tests/ directory.
 function (lsf_build_test_cc test_name)
 	add_executable (${test_name}
-		tests/utils.c
-		tests/${test_name}.cc
+		${CMAKE_CURRENT_SOURCE_DIR}/tests/utils.c
+		${CMAKE_CURRENT_SOURCE_DIR}/tests/${test_name}.cc
 		)
 	target_link_libraries (${test_name} sndfile)
 	set_target_properties (${test_name}
